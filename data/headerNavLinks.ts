4f423937a883ce8c59c882480191a0426bd528ce
rseghier <65013339+rseghier@users.noreply.github.com>
2024-06-01 19:13:45 +0800

    Add new version of website

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,10 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
+  { href: '/blog', title: 'ESG News' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
 ]
 
 export default headerNavLinks
+
+//   { href: '/projects', title: 'Projects' },
